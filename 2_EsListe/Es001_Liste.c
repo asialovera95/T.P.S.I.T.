404126e0321faaf646ed15ccb37cb0d1031194e5
Asia Lovera <48619341+asialovera95@users.noreply.github.com>
2019-11-19 09:36:20 +0100

    Update Es001_Liste.c

--- 2_EsListe/Es001_Liste.c
+++ 2_EsListe/Es001_Liste.c
@@ -22,17 +22,17 @@ void stampaLista(ListaDiElementi l) {
 */
 //--------------------------------------------
 
-/* esercizio 2 */
+// esercizio 2
 void stampaLista ( ListaDiElementi l ){
-    if (l == NULL ) printf (" //\ n" );
+    if (l == NULL ) printf (" \n" );
     else{
         printf ( "%d -> " ,l - > valore );
         stampaLista (l -> next );
     }
 }
 
-/* esercizio 3*/
 
+// esercizio 3
 int lunghezzaListaIterativa ( ListaDiElementi l){
     int lung = 0;
     while (l != NULL ){
@@ -46,6 +46,102 @@ int lunghezzaListaRicorsiva ( ListaDiElementi l){
     if (l == NULL ) return 0;
     else return lunghezzaListaRicorsiva (l -> next )+1;
 }
+
+
+// esercizio 4
+void deallocaLista(ListaDiElementi 1){
+    if(l != NULL){
+        deallocaLista(l -> next);
+        free (l);
+        /*Il prototipo della funzione free in C è: void free(void *); 
+        Quindi la funzione ha come argomento il puntatore all'inizio dell'area di memoria precedentemente allocata, 
+        che adesso si vuole rendere disponibile perchè non più utilizzata. La funzione inoltre non ha alcun valore di ritorno*/
+    }
+}
+
+// esercizio 5
+void ordinaLista(ListaDiElementi l) { 
+    ListaDiElementi l1,tmp,prec_tmp;
+
+    if ((l==NULL)||(l->next==NULL)) return l;
+    else l1 = ordinaLista(l->next); 
+    // l1 e’ di sicuro non vuota
+
+    tmp = l1; 
+    prec_tmp = l; 
+
+    //l’ordine delle condizioni in && e’ importante!
+    while ((tmp!=NULL)&&(tmp->info <l->info)) { 
+        prec_tmp=tmp; 
+        tmp=tmp->next; 
+    }
+
+    // devo piazzare il primo elemento di l dentro a l1 
+    if (tmp==NULL){ //inserimento in coda  
+        l->next=NULL; 
+        prec_tmp ->next=l;
+        return l1;
+    }else{
+        if (tmp==l1){ //inserimento in testa 
+            l->next=l1; 
+            return l; 
+        }else{ //inserimento in mezzo 
+            l->next = tmp; 
+            prec_tmp ->next = l; 
+            return l1; 
+        }
+    }
+} 
+
+// Esercizio 6
+void merge ( ListaDiElementi l1 , ListaDiElementi l2 ){
+    ListaDiElementi risultato = NULL ;
+    ListaDiElementi new = NULL ;
+    int primavolta = 1;
+    /* scorre le due liste simultaneamente */
+    while (( l1 != NULL )&&( l2 != NULL )) {
+        if ( primavolta ){
+            new = ( ListaDiElementi ) malloc ( sizeof ( ElementoDiLista ));
+            risultato = new ;
+            primavolta =0;
+        }
+        else{
+            new -> next = ( ListaDiElementi ) malloc ( sizeof ( ElementoDiLista ));
+            new = new -> next ;
+        }
+
+        if (l1 -> info <l2 -> info ){ 
+            new - > info =l1 - > info ; 
+            l1 =l1 - > next ; 
+        }
+        else{ 
+            new - > info =l2 - > info ; 
+            l2 =l2 - > next ; 
+        }
+    }
+
+    /* uno (e solo uno) dei seguenti due cicli viene eseguito */
+    while ( l1 != NULL ){
+        new -> next = ( ListaDiElementi ) malloc ( sizeof ( ElementoDiLista ));
+        new = new -> next ;
+        new -> info = l1 -> info ;
+        l1 = l1 - > next ;
+    }
+
+    while ( l2 != NULL ){
+        new -> next = ( ListaDiElementi ) malloc ( sizeof ( ElementoDiLista ));
+        new = new -> next ;
+        new -> info = l2 -> info ;
+        l2 = l2 - > next ;
+    }
+
+    /* mette NULL in fondo alla lista */
+    if ( new != NULL ) new -> next = NULL ;
+    return risultato ;
+}
+
+
+
 
 
 int main(){
@@ -86,5 +182,14 @@ int main(){
             l=l->next; /* il puntatore corrente è assegnato al puntatore all'elemento successivo */
         }
         printf("\n");
+
+        //chiamate alle funzioni
+        stampaLista(ListaDiElementi);
+        deallocaLista(ListaDiElementi);
+        ordinaLista(ListaDiElementi);
+        merge(ListaDiElementi, ListaDiElementi);
+        
         return 0;
+
 }
+
